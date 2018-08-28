1115fd83228430b79621a5b4f79a2c74f1017c1f
Nayton Batista <naytonbatista@gmail.com>
2018-08-28 00:50:25 -0300

    Hooks na prática

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,36 +1,39 @@
-describe('Main', function(){
-    describe('Método A', function(){
-        context('Caso 1', function(){
-            it('deve acontecer alguma coisa', function(){
-                //espera o que aconteca
-                //Entra de dado / método
-                //Espera retornar
-                throw new Error('Apenas um erro.');
-            })
-        })
+describe('Main', function () {
 
-        context.only('Caso 2', function(){
-            it('deve acontecer alguma coisa', function(){
-                //espera o que aconteca
-                //Entra de dado / método
-                //Espera retornar
-                throw new Error('Apenas um erro.');
-                
-            })
+    var arr;
+    //Roda uma vez antes do teste
+    before(function () {
+        //iniciar uma conexão com o banco de dados.
+        //criar um conjunto de dados
+    });
 
-            it('deve acontecer algum mimimi', function(){
-                //espera o que aconteca
-                //Entra de dado / método
-                //Espera retornar
-                throw new Error('Apenas um erro.');
+    //Roda uma vez depois do teste
+    after(function () {
+        //fecha conexão com o banco de dados.
+        //apaga um conjunto de dados.
+    });
 
-            })
-        })
+    //Roda todas as vezes, antes de CADA bloco
+    beforeEach(function () {
+        arr = [1,2,3]
+    });
 
+    //Roda todas as vezes, depois de CADAS bloco
+    afterEach(function () {
 
-    })
+    });
 
-    describe('Método B', function(){
-        
-    })
+    it('deve ter o tamanho de 4 quando adicionar um valor no array', function () {
+        arr.push(4);
+        console.log(arr.length) //4 
+    });
+
+    it('deve ter o tamanho de 2 quando excluir um valor do array', function () {
+        arr.pop();
+        console.log(arr.length) //2
+    });
+
+    it('deve ter o valor 3 quando usar o pop no array', function () {
+        console.log(arr.pop() === 3) //true
+    });
 })
