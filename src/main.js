ca83512140f8b43fc1db2991a26e96a3edee292a
Nayton Batista <naytonbatista@gmail.com>
2018-08-29 00:30:03 -0300

    Refactory to ES6

--- src/main.js
+++ src/main.js
@@ -1,19 +1,8 @@
-module.exports = {
-  soma(num1, num2) {
-    return num1 + num2;
-  },
+const soma = (num1, num2) => num1 + num2;
+const subtracao = (num1, num2) => num1 - num2;
+const divisao = (num1, num2) => (num2 === 0 ? 'Não é possível dividir por 0' : num1 / num2);
+const multiplicacao = (num1, num2) => num1 * num2;
 
-  subtracao(num1, num2) {
-    return num1 - num2;
-  },
-
-  divisao(num1, num2) {
-
-    return num2 === 0 ? "Não é possível dividir por 0" : num1 / num2;
-
-  },
-
-  multiplicacao(num1, num2) {
-    return num1 * num2;
-  },
+export {
+  soma, subtracao, divisao, multiplicacao,
 };
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,68 +1,65 @@
-var expect = require('chai').expect;
-var calc = require('../src/main.js');
-describe('Calc', function () {
+import { expect } from 'chai';
+import {soma, divisao, multiplicacao, subtracao} from '../src/main';
+
+describe('Calc', () => {
 
     //Smoke tests
-    describe('Smoke tests', function () {
+    describe('Smoke tests', () => {
 
-        it('deve existir a biblioteca calculadora', function () {
-            expect(calc).to.exist;
-        });
-
-        it('deve existir o método "soma"', function () {
-            expect(calc.soma).to.exist;
-            expect(calc.soma).to.be.a.funtion;
+        it('deve existir o método "soma"', () => {
+            expect(soma).to.exist;
+            expect(soma).to.be.a.funtion;
 
         });
 
-        it('deve existir o método "subtracao"', function () {
-            expect(calc.subtracao).to.exist;
-            expect(calc.subtracao).to.be.a.funtion;
+        it('deve existir o método "subtracao"', () => {
+            expect(subtracao).to.exist;
+            expect(subtracao).to.be.a.funtion;
 
         });
 
-        it('deve existir o método "divisao"', function () {
-            expect(calc.divisao).to.exist;
-            expect(calc.divisao).to.be.a.funtion;
+        it('deve existir o método "divisao"', () => {
+            expect(divisao).to.exist;
+            expect(divisao).to.be.a.funtion;
 
         });
 
-        it('deve existir o método "multiplicacao"', function () {
-            expect(calc.multiplicacao).to.exist;
-            expect(calc.multiplicacao).to.be.a.funtion;
+        it('deve existir o método "multiplicacao"', () => {
+            expect(multiplicacao).to.exist;
+            expect(multiplicacao).to.be.a.funtion;
 
         });
     });
 
-    describe('Soma', function () {
-        it('deve retornar 4 quando soma(2,2)', function () {
-            expect(calc.soma(2,2)).to.be.equal(4);
+    describe('Soma', () => {
+        it('deve retornar 4 quando soma(2,2)', () => {
+            expect(soma(2, 2)).to.be.equal(4);
         });
     });
 
-    describe('Subtracao', function () {
-        it('deve retornar 0 quando subtracao(2,2)', function () {
-            expect(calc.subtracao(2,2)).to.be.equal(0);
+    describe('Subtracao', () => {
+        it('deve retornar 0 quando subtracao(2,2)', () => {
+            expect(subtracao(2, 2)).to.be.equal(0);
         });
 
-        it('deve retornar -4 quando subtracao(6,10)', function () {
-            expect(calc.subtracao(6,10)).to.be.equal(-4);
-        });
-    });
-
-    describe('Multiplicação', function () {
-        it('deve retornar 10 quando multiplicacao(2,5)', function () {
-            expect(calc.multiplicacao(2,5)).to.be.equal(10);
+        it('deve retornar -4 quando subtracao(6,10)', () => {
+            expect(subtracao(6, 10)).to.be.equal(-4);
         });
     });
 
-    describe('Divisão', function () {
-        it('deve retornar 1 quando divisão(2,2)', function () {
-            expect(calc.divisao(2,2)).to.be.equal(1);
+    describe('Multiplicação', () => {
+        it('deve retornar 10 quando multiplicacao(2,5)', () => {
+            expect(multiplicacao(2, 5)).to.be.equal(10);
+        });
+    });
+
+    describe('Divisão', () => {
+        it('deve retornar 1 quando divisão(2,2)', () => {
+            expect(divisao(2, 2)).to.be.equal(1);
         });
 
-        it('deve retornar "Não é possível dividir por 0" quando dividir por 0', function () {
-            expect(calc.divisao(4,0)).to.be.equal("Não é possível dividir por 0");
+        it('deve retornar "Não é possível dividir por 0" quando dividir por 0', () => {
+            expect(divisao(4, 0)).to.be.equal("Não é possível dividir por 0");
         });
     });
 });
